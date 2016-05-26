9aac597b836a5f3754f183e80a789410873b147e
venizeng <venizeng@gmail.com>
2016-05-26 17:23:07 +0800

    加入日期工具

--- src/main/java/com/lifeix/football/common/util/DateUtil.java
+++ src/main/java/com/lifeix/football/common/util/DateUtil.java
@@ -6,15 +6,18 @@ import java.util.Date;
 
 public class DateUtil {
 
-    private static final String DATE_FORMAT_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATETIME_FORMAT_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+    
+    private static final String DATE_FORMAT_DEFAULT = "yyyy-MM-dd";
 
-    public static String toDate(Date d) {
+    public static String toDateStr(Date d) {
         DateFormat format = new SimpleDateFormat(DATE_FORMAT_DEFAULT);
         return format.format(d);
     }
-
-    public static String toDate(Date d, String form) {
-        DateFormat format = new SimpleDateFormat(form);
+    
+    public static String toTimeStr(Date d) {
+        DateFormat format = new SimpleDateFormat(DATETIME_FORMAT_DEFAULT);
         return format.format(d);
     }
+
 }
