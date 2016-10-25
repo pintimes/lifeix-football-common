28dfe9cc1788df7bb500b17c0af04312ca61a089
l99xl <lex@l99.com>
2016-10-25 14:10:23 +0800

    新增：用户常量类UserConst.java

---
+++ src/main/java/com/lifeix/football/common/consts/UserConst.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.lifeix.football.common.consts;
+
+/**
+ * @author xule
+ */
+public class UserConst {
+	public static final String USER_REGESTER_SOURCE_CF = "中国足球网";
+	public static final String USER_REGESTER_SOURCE_CFCS = "中国足球网赛事系统";
+}
