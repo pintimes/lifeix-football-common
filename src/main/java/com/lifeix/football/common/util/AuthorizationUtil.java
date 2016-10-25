4b95ad099d038d3472a472f0557783b941594f3e
chaochaog <chaochaog@l99.com>
2016-10-25 16:04:10 +0800

    dd

--- src/main/java/com/lifeix/football/common/util/AuthorizationUtil.java
+++ src/main/java/com/lifeix/football/common/util/AuthorizationUtil.java
@@ -1,7 +1,5 @@
 package com.lifeix.football.common.util;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.util.StringUtils;
 
 import com.lifeix.football.common.exception.AuthorizationException;
@@ -18,40 +16,31 @@ public class AuthorizationUtil {
      * @param groups
      */
     public static void adminAuthorization(String groups) {
-        if (StringUtils.isEmpty(groups)||!groups.contains("admin")) {
+        if (StringUtils.isEmpty(groups) || !groups.contains("admin")) {
             throw new AuthorizationException();
         }
-        //HttpServletRequest currentRequest = BaseApi.getCurrentRequest();
-    	/**
-    	 * 内网进入的认为是管理员，admin不通过网关传入
-    	 * 从前置获得header from字段，如果有值则表示是从kong过来的即不是一个Admin
-    	 */
-    	//String from = currentRequest.getHeader("from");
-    	//if (!StringUtils.isEmpty(from)) {
-		//	throw new AuthorizationException();
-		//}
+        // HttpServletRequest currentRequest = BaseApi.getCurrentRequest();
+        /**
+         * 内网进入的认为是管理员，admin不通过网关传入 从前置获得header from字段，如果有值则表示是从kong过来的即不是一个Admin
+         */
+        // String from = currentRequest.getHeader("from");
+        // if (!StringUtils.isEmpty(from)) {
+        // throw new AuthorizationException();
+        // }
     }
-    
-    
+
     /**
-     * @name userAdminAuthorization
-     * @description 管理员或用户授权，groups必须包含user或者admin才能通过授权
-     * @author xule
-     * @version 2016年8月24日 上午10:06:14
-     * @param 
-     * @return void
-     * @throws
+     * @name userAdminAuthorization @description 管理员或用户授权，groups必须包含user或者admin才能通过授权 @author
+     *       xule @version 2016年8月24日 上午10:06:14 @param @return void @throws
      */
     public static void userAdminAuthorization(String groups) {
-        if (StringUtils.isEmpty(groups)||!(groups.contains("user")||groups.contains("admin"))) {
-        	throw new AuthorizationException();
+        if (StringUtils.isEmpty(groups) || !(groups.contains("user") || groups.contains("admin"))) {
+            throw new AuthorizationException();
         }
     }
-    
+
     /**
-     * 用户简单授权
-     * groups中包含了admin字段即可通过
-     * groups中包含了user字段kongId=userid即可通过
+     * 用户简单授权 groups中包含了admin字段即可通过 groups中包含了user字段kongId=userid即可通过
      * 
      * @description
      * @author zengguangwei
@@ -59,56 +48,45 @@ public class AuthorizationUtil {
      *
      * @param groups
      */
-    public static void userAdminAuthorization(String groups,String kongId,String userId) {
+    public static void userAdminAuthorization(String groups, String kongId, String userId) {
         if (StringUtils.isEmpty(groups)) {
             throw new AuthorizationException();
         }
         if (groups.contains("admin")) {
-        	return;
+            return;
         }
-        if (groups.contains("user")&&userId.equals(kongId)) {
+        if (groups.contains("user") && userId.equals(kongId)) {
             return;
         }
         throw new AuthorizationException();
     }
-    
-  /**
-   * @name userAuthorization
-   * @description 用户授权认证
-   * @author xule
-   * @version 2016年9月23日 下午2:11:29
-   * @param 
-   * @return void
-   * @throws
-   */
-    public static void userAuthorization(String groups,String kongId,String userId) {
-    	if (StringUtils.isEmpty(groups)) {
-    		throw new AuthorizationException();
-    	}
-    	if (groups.contains("user")&&userId.equals(kongId)) {
-    		return;
-    	}
-    	throw new AuthorizationException();
+
+    /**
+     * @name userAuthorization @description 用户授权认证 @author xule @version 2016年9月23日
+     *       下午2:11:29 @param @return void @throws
+     */
+    public static void userAuthorization(String groups, String kongId, String userId) {
+        if (StringUtils.isEmpty(groups)) {
+            throw new AuthorizationException();
+        }
+        if (groups.contains("user") && userId.equals(kongId)) {
+            return;
+        }
+        throw new AuthorizationException();
     }
 
-
-	/**
-	 * @name monitorAuthorization
-	 * @description
-	 * @author xule
-	 * @version 2016年10月18日 上午10:09:59
-	 * @param 
-	 * @return void
-	 * @throws 
-	 */
-	public static void monitorAuthorization(String groups) {
-		if (StringUtils.isEmpty(groups)) {
-    		throw new AuthorizationException();
-    	}
-    	if (groups.contains("monitor")) {
-    		return;
-    	}
-    	throw new AuthorizationException();
-	}
+    /**
+     * @name monitorAuthorization @description @author xule @version 2016年10月18日
+     *       上午10:09:59 @param @return void @throws
+     */
+    public static void monitorAuthorization(String groups) {
+        if (StringUtils.isEmpty(groups)) {
+            throw new AuthorizationException();
+        }
+        if (groups.contains("monitor")) {
+            return;
+        }
+        throw new AuthorizationException();
+    }
 
 }
