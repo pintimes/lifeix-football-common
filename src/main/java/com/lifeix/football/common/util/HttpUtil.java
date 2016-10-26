5680500a03a2ae15b07e8164fa01274f5d802ac6
chaochaog <chaochaog@l99.com>
2016-10-26 17:21:45 +0800

    aa

--- src/main/java/com/lifeix/football/common/util/HttpUtil.java
+++ src/main/java/com/lifeix/football/common/util/HttpUtil.java
@@ -20,63 +20,63 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 public class HttpUtil {
 
-	public static String sendDelete(String url) throws Exception {
-		HttpDelete http = new HttpDelete(url);
-		return sendHttp(http);
-	}
-	
-	public static String sendGet(String url) throws Exception {
-		HttpGet http = new HttpGet(url);
-		return sendHttp(http);
-	}
+    public static String sendDelete(String url) throws Exception {
+        HttpDelete http = new HttpDelete(url);
+        return sendHttp(http);
+    }
 
-	public static String sendPost(String url, Map<String, String> map) throws Exception {
-		HttpPost http = new HttpPost(url);
-		http.setEntity(getEntity(map));
-		return sendHttp(http);
-	}
+    public static String sendGet(String url) throws Exception {
+        HttpGet http = new HttpGet(url);
+        return sendHttp(http);
+    }
 
-	public static String sendPut(String url, Map<String, String> map) throws Exception {
-		HttpPut http = new HttpPut(url);
-		http.setEntity(getEntity(map));
-		return sendHttp(http);
-	}
+    public static String sendPost(String url, Map<String, String> map) throws Exception {
+        HttpPost http = new HttpPost(url);
+        http.setEntity(getEntity(map));
+        return sendHttp(http);
+    }
 
-	private static UrlEncodedFormEntity getEntity(Map<String, String> map) throws UnsupportedEncodingException {
-		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-		if (!CollectionUtils.isEmpty(map)) {
-			Set<String> set = map.keySet();
-			for (String key : set) {
-				urlParameters.add(new BasicNameValuePair(key, map.get(key)));
-			}
-		}
-		return new UrlEncodedFormEntity(urlParameters, "utf-8");
-	}
+    public static String sendPut(String url, Map<String, String> map) throws Exception {
+        HttpPut http = new HttpPut(url);
+        http.setEntity(getEntity(map));
+        return sendHttp(http);
+    }
 
-	@SuppressWarnings("resource")
-	public static String sendHttp(HttpUriRequest request) throws Exception {
-		HttpClient client = new DefaultHttpClient();
-		HttpResponse response = client.execute(request);
-		if (response.getEntity()==null) {
-			return null;
-		}
-		if (StringUtils.isEmpty(response.getEntity().getContent())) {
-			return null ;
-		}
-		InputStream is = response.getEntity().getContent();
-		BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-		return result.toString();
-	}
-	
+    private static UrlEncodedFormEntity getEntity(Map<String, String> map)
+            throws UnsupportedEncodingException {
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        if (!CollectionUtils.isEmpty(map)) {
+            Set<String> set = map.keySet();
+            for (String key : set) {
+                urlParameters.add(new BasicNameValuePair(key, map.get(key)));
+            }
+        }
+        return new UrlEncodedFormEntity(urlParameters, "utf-8");
+    }
+
+    @SuppressWarnings("resource")
+    public static String sendHttp(HttpUriRequest request) throws Exception {
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(request);
+        if (response.getEntity() == null) {
+            return null;
+        }
+        if (StringUtils.isEmpty(response.getEntity().getContent())) {
+            return null;
+        }
+        InputStream is = response.getEntity().getContent();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+
 }
