886e6835ce8f38eb64cb18f1e67e0c85c1c0afb2
l99xl <lex@l99.com>
2016-11-01 19:53:02 +0800

    回滚2个版本，放弃将用户高频信息存放到kong DB中的做法

--- src/main/java/com/lifeix/football/common/util/APIGatewayUtil.java
+++ src/main/java/com/lifeix/football/common/util/APIGatewayUtil.java
@@ -7,13 +7,10 @@ import org.springframework.util.StringUtils;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.lifeix.football.common.exception.BusinessException;
 import com.lifeix.football.common.exception.IllegalparamException;
 import com.lifeix.football.common.model.Consumer;
 /**
  * API网关注册
- * 
- * 网关用户信息中consumer_id存放用户高频访问信息，username存放用户系统中对应用户的id
  * @author zengguangwei
  *
  */
@@ -30,19 +27,6 @@ public class APIGatewayUtil {
 	 * @throws Exception 
 	 */
 	public static String registToAPIGateway(String host, String userId,String group) throws Exception {
-		return registToAPIGateway(host, userId, group, null, null, null, null);
-	}
-	
-	/**
-	 * 注册用户到API网关 返回用户的auth key，用户不存在时创建用户到网管DB然后再获取key；用户存在则直接获取key，此处不修改kong DB中用户信息
-	 * @name registToAPIGateway
-	 * @author zengguangwei,xule
-	 * @version 2016年4月19日上午10:17:10,2016年11月1日 上午9:23:25
-	 * @param avatar,nickname,gender,label,如要在Kong DB中存储用户信息，则此4个参数是必需的
-	 * @return String
-	 * @throws Exception
-	 */
-	public static String registToAPIGateway(String host, String userId,String group,String avatar,String nickname,Integer gender,String label) throws Exception {
 		if (StringUtils.isEmpty(host)) {
 			throw new IllegalparamException("host is empty");
 		}
@@ -52,26 +36,8 @@ public class APIGatewayUtil {
 		if (StringUtils.isEmpty(group)) {
 			throw new IllegalparamException("consumer.group is empty");
 		}
-		String custom_id;
+		String custom_id=userId;
 		String username = userId;//username存储userId，consumer_id存储用户高频访问信息
-		if (StringUtils.isEmpty(avatar)) {
-			custom_id = userId;
-		}else{
-			JSONObject json=new JSONObject();
-			json.put("id", userId);
-			json.put("avatar", avatar);
-			if (!StringUtils.isEmpty(nickname)) {
-				json.put("nickname", nickname);
-			}
-			if (gender==1||gender==2) {
-				json.put("gender", gender);
-			}
-			if (!StringUtils.isEmpty(label)) {
-				json.put("label", label);
-			}
-			custom_id=json.toJSONString();
-		}
-		
 		/**
 		 * 检索Consumer
 		 */
@@ -101,25 +67,7 @@ public class APIGatewayUtil {
 		return key;
 	}
 	
-	/**
-	 * @name updateConsumer
-	 * @description 更新consumer信息，在用户注册或登录通过Kong时，对用户consumer的consumer_id信息进行更新
-	 * @author xule
-	 * @version 2016年10月31日 下午7:21:15
-	 * @param 
-	 * @return void
-	 * @throws
-	 */
-	public static void updateConsumer(String host,String username, String custom_id){
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("custom_id", custom_id);
-		try {
-			HttpUtil.sendPatch(host + "/consumers/" + username,params);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new BusinessException("更新consumer失败  "+e.getMessage());
-		}
-	}
+	
 
 	/**
 	 * 删除Consumer 同时关联删除Key以及Group等
